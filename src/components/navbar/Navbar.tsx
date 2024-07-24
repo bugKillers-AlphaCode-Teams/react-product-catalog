6463f8d70833a308dcf23c23db8c0af788a712e4
Humen Vasyl <vasyahumenit@gmail.com>
2024-07-24 09:48:15 +0300

    start header

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,29 +3,51 @@ import styles from "./Navbar.module.scss";
 
 export const Navbar = () => {
   return (
-    <nav>
-      <ul className={styles.navbar}>
-        <li>
+    <div className={styles.header}>
+      <div className={styles.header__container}>
+        <div className={styles.header__content}>
           <NavLink to="/" className="">
-            Home
+            <img src="/img/Logo.svg" alt="logo" className={styles.logo} />
           </NavLink>
-        </li>
-        <li>
-          <NavLink className="" to="phone">
-            Phones
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="" to="tablets">
-            Tablets
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="" to="accessories">
-            accessories
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+          <nav className="">
+            <ul className={styles.navbar}>
+              <li>
+                <NavLink to="/" className={styles.nav__link}>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className={styles.nav__link} to="phone">
+                  Phones
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className={styles.nav__link} to="tablets">
+                  Tablets
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className={styles.nav__link} to="accessories">
+                  accessories
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+          <div className={styles.header__icon}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className={styles.buttons}>
+            <NavLink to="favourit" className={styles.cart_and_like}>
+              <img src="/img/favourit.svg" alt="favourit" />
+            </NavLink>
+            <NavLink to="shoping" className={styles.cart_and_like}>
+              <img src="/img/shoping.svg" alt="shoping" />
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
