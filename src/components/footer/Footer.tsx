e8765c2528aab8f59ed1324e81d93efcaa9b0eec
Yaroslav Leshchenko <yaroslavleshenkoforwork@gmail.com>
2024-07-26 22:20:58 +0300

    footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,3 +1,41 @@
-export const Footer = () => {
-  return <div>Footer</div>;
+import React from "react";
+import styles from "./Footer.module.scss";
+import logo from "../../images/icons/Logo.svg";
+import chevron from "../../images/icons/Chevron (Arrow Right).svg";
+
+export const Footer: React.FC = () => {
+  return (
+  <div className={styles.footer}>
+      <div className={styles.footer__wrapper}>
+       <div className={styles.footer__logo}>
+          <img src={logo} className={styles["footer__logo-image"]} alt="logo" />
+        </div>
+
+      <div className={styles.footer__items}>
+        <ul className={styles["footer__items-links"]}>
+          <li>
+            <a href="#" className={styles["footer__items-item"]}>Github</a>
+          </li>
+          <li>
+            <a href="#" className={styles["footer__items-item"]}>Contacts</a>
+          </li>
+          <li>
+            <a href="#" className={styles["footer__items-item"]}>Rights</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className={styles["footer__goup"]}>
+        <a href="" className={styles["footer__goup-text"]}>Back to top button 
+          <div className={styles["footer__goup-chevron"]}>
+            <div className={styles["footer__goup-wrapper"]}>
+             <img src={chevron} alt="chevron" />
+            </div>
+          </div>
+        </a>
+      </div>
+
+    </div>
+  </div>
+  );
 };
