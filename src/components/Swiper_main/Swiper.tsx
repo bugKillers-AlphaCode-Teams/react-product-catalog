3a12c859ad6a62dfefe2d91532b8c00520128d03
Sa4ok <sasha.rusinka@gmail.com>
2024-07-29 13:07:48 +0300

    Anti-Fix Problem with swiper

--- src/components/Swiper_main/Swiper.tsx
+++ src/components/Swiper_main/Swiper.tsx
@@ -1,48 +1,73 @@
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css';
-import SwiperCore from 'swiper/core';
-import './Swiper.scss';
 
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import styles from './Swiper.module.scss';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import { Order } from './Order Now/Order';
 
-
-import { A11y, Navigation, Pagination, Scrollbar } from 'swiper/modules';
-
-SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
-
-const SimpleSlider = () => {
+export const Slider: React.FC = () => {
   return (
-    
-    <div className="swiper-container">
-      <h1 className='header'>Welcome to Nice Gadgets store!</h1>
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-        navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        }}
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-      >
-        <SwiperSlide>
-          <img src="src\images\icons\Banner.png" alt="Slide 1" style={{ width: '100%' }} />
-        </SwiperSlide>
-        <SwiperSlide>
-          {/* <img src="src\images\icons\t.PNG" alt="Slide 2" style={{ width: '100%'}} /> */}
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/path/to/your/image3.jpg" alt="Slide 3" style={{ width: '100%' }} />
-        </SwiperSlide>
-      </Swiper>
-      {/* Власні кнопки навігації */}
-      <div className="swiper-button-prev">
-        <img src="src\images\icons\Chevron (Arrow Left).png" alt="Overlay" className="overlay-image" />
-      </div>
-      <div className="swiper-button-next">
-        <img src="src\images\icons\Chevron (Arrow Right).png" alt="Next" />
+    <div>
+      <section className={`${styles.page__main} ${styles.main}`}>
+        <h1 className={styles.main__title}>Welcome to Nice Gadgets store!</h1>
+      </section>
+
+      <div className={`main__swiper ${styles.swiper}`}>
+        <Swiper
+          modules={[Autoplay, Pagination, Navigation]}
+          spaceBetween={0}
+          slidesPerView={1}
+          loop={true}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          navigation={{
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          }}
+          pagination={{
+            type: 'bullets',
+            el: '.swiper-pagination',
+            clickable: true,
+          }}
+        >
+          <SwiperSlide className={styles.swiper__slide}>
+            <Order />
+            <a
+              className={`${styles.swiper__image} ${styles[`swiper__image--1`]}`}
+            ></a>
+          </SwiperSlide>
+          <SwiperSlide className={styles.swiper__slide}>
+            <div
+              className={`${styles.swiper__wrapper} ${styles['swiper__wrapper--1']} `}
+            >
+              <a
+                className={`${styles.swiper__image} ${styles[`swiper__image--2`]}`}
+              ></a>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={styles.swiper__slide}>
+            <div
+              className={`${styles.swiper__wrapper} ${styles['swiper__wrapper--2']} `}
+            >
+              <a
+                className={`${styles.swiper__image} ${styles[`swiper__image--3`]}`}
+              ></a>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+        <div className="button-arrangment">
+          <div className="button-swiper">
+            <button className="swiper-button-prev"></button>
+            <button className="swiper-button-next"></button>
+            <button className="swiper-pagination"></button>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
-
-export default SimpleSlider;
+};
