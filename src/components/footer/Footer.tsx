b1383b371b39585ff2deeb3c111a0ad25b40f583
Yaroslav Leshchenko <yaroslavleshenkoforwork@gmail.com>
2024-07-29 18:11:27 +0300

    before merge

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,38 +4,45 @@ import logo from "../../images/icons/Logo.svg";
 import chevron from "../../images/icons/Chevron (Arrow Right).svg";
 
 export const Footer: React.FC = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
   return (
-  <div className={styles.footer}>
+    <div className={styles.footer}>
       <div className={styles.footer__wrapper}>
-       <div className={styles.footer__logo}>
+        <div className={styles.footer__logo}>
           <img src={logo} className={styles["footer__logo-image"]} alt="logo" />
         </div>
 
-      <div className={styles.footer__items}>
-        <ul className={styles["footer__items-links"]}>
-          <li>
-            <a href="#" className={styles["footer__items-item"]}>Github</a>
-          </li>
-          <li>
-            <a href="#" className={styles["footer__items-item"]}>Contacts</a>
-          </li>
-          <li>
-            <a href="#" className={styles["footer__items-item"]}>Rights</a>
-          </li>
-        </ul>
-      </div>
+        <div className={styles.footer__items}>
+          <ul className={styles["footer__items-links"]}>
+            <li>
+              <a href="#" className={styles["footer__items-item"]}>Github</a>
+            </li>
+            <li>
+              <a href="#" className={styles["footer__items-item"]}>Contacts</a>
+            </li>
+            <li>
+              <a href="#" className={styles["footer__items-item"]}>Rights</a>
+            </li>
+          </ul>
+        </div>
 
-      <div className={styles["footer__goup"]}>
-        <a href="" className={styles["footer__goup-text"]}>Back to top button 
-          <div className={styles["footer__goup-chevron"]}>
-            <div className={styles["footer__goup-wrapper"]}>
-             <img src={chevron} alt="chevron" />
+        <div className={styles["footer__goup"]} onClick={scrollToTop}>
+          <span className={styles["footer__goup-text"]}>
+            Back to top button
+            <div className={styles["footer__goup-chevron"]}>
+              <div className={styles["footer__goup-wrapper"]}>
+                <img src={chevron} alt="chevron" />
+              </div>
             </div>
-          </div>
-        </a>
+          </span>
+        </div>
       </div>
-
     </div>
-  </div>
   );
 };
