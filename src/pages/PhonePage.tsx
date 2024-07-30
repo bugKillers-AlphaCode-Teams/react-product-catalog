971364a840d9f0b2f9079b092a0c00f7edf049fd
MariCodec <masha1994la@gmail.com>
2024-07-30 17:44:59 +0300

    locale-storage

--- src/pages/PhonePage.tsx
+++ src/pages/PhonePage.tsx
@@ -2,20 +2,24 @@ import { useEffect, useState } from "react";
 import { ProductList } from "../components/ProductList/ProductList";
 import { Product } from "../types/Product";
 import { Pagination } from "../components/Pagination/Pagination.tsx";
+import { fetchPhones } from "../services/phoneService.ts";
 
-const URL = "/api/phones.json";
 export const PhonePage = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const productsPerPage = 16;
 
   useEffect(() => {
-    fetch(URL)
-      .then((response) => response.json())
-      .then((phones) => setProducts(phones))
-      .catch(() => {
-        "some error";
-      });
+    const fetchData = async () => {
+      try {
+        const phones = await fetchPhones();
+        setProducts(phones);
+      } catch (error) {
+        console.error("Failed to fetch phones:", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   const lastProductIndex = currentPage * productsPerPage;
