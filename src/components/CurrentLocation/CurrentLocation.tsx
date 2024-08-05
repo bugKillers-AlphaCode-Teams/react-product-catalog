79339c7bb3186687c70d4907db65a06c3debe586
MariCodec <masha1994la@gmail.com>
2024-08-05 12:31:23 +0300

    fixed  fillRule

--- src/components/CurrentLocation/CurrentLocation.tsx
+++ src/components/CurrentLocation/CurrentLocation.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from "react-i18next";
 export const CurrentLocation = () => {
   const { pathname } = useLocation();
   const currentLocation = pathname.slice(1);
-   const { t } = useTranslation();
+  const { t } = useTranslation();
   const translatedLocation = t(`location.${currentLocation}`);
   return (
     <div className={styles.currentLocation}>
@@ -21,8 +21,8 @@ export const CurrentLocation = () => {
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M5.52876 3.52851C5.78911 3.26816 6.21122 3.26816 6.47157 3.52851L10.4716 7.52851C10.7319 7.78886 10.7319 8.21097 10.4716 8.47132L6.47157 12.4713C6.21122 12.7317 5.78911 12.7317 5.52876 12.4713C5.26841 12.211 5.26841 11.7889 5.52876 11.5285L9.05735 7.99992L5.52876 4.47132C5.26841 4.21097 5.26841 3.78886 5.52876 3.52851Z"
           fill="#4A4D58"
         />
